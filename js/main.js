c20247077c82a9036b8661d362bd4bf6bcc32c9e
Alan <akillian@outlook.com>
2017-03-29 13:56:28 -0400

    Commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 var el = document.getElementById("req_btn");
-
 el.addEventListener("click", function () {
     //Retrieve text from input box
     var searchValue = document.getElementById('input_value').value;
-    if (searchValue !== '') {
-        console.log(searchValue);
+    if (searchValue.length === 0) {
+        console.log("No Search Value!!!");
+        return;
     }
     else {
-        console.log("No Search Value!!!")
+        // AJAX Request
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                str = this.responseText;
+                console.log(str);
+            }
+        }
     }
+    xhr.open("POST", "http://www.youtubeinmp3.com/r.php?q=" + searchValue, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send();
 });
