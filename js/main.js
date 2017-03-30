57b77a610991e90b33093f058f099f62f2310df4
Alan <akillian@outlook.com>
2017-03-30 14:00:08 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-var el = document.getElementById("req_btn");
-el.addEventListener("click", function () {
+var oEl = document.getElementById("input_value");
+oEl.addEventListener("focus", function (e) {
     //Retrieve text from input box
-    var searchValue = document.getElementById('input_value').value;
-    if (searchValue.length === 0) {
-        console.log("No Search Value!!!");
-        return;
-    }
-    else {
-        // AJAX Request
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                str = this.responseText;
-                console.log(str);
-            }
-        }
-    }
-    xhr.open("POST", "http://www.youtubeinmp3.com/r.php?q=" + searchValue, true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send();
+    e.target.style.background = "#E6F0F9";
 });
+
+
+oEl.addEventListener("blur", function (e) {
+    var searchValue = document.getElementById('input_value').value;
+    e.target.style.background = "#FFFFFF";
+
+    var oDwnLdEl = document.querySelector('a[href*= "//www.youtubeinmp3.com/fetch/"]');
+    var hrefValue = oDwnLdEl.getAttribute("href");
+    hrefValue += searchValue;
+
+    oDwnLdEl.setAttribute("href",hrefValue);
+
+});
+
+
+
